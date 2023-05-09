6bd109e0bba16f0a87a233eaf9899ae1a607649f
Afrina <afrinarahaman07@gmail.com>
2023-05-09 08:47:56 +0200

    change

--- src/main.cpp
+++ src/main.cpp
@@ -350,7 +350,7 @@ void loop() {
   */
     // Temperature in Celsius degrees 
     float temperature = sensors.getTempCByIndex(0);
-    Serial.print("Current room temperature: ");
+    Serial.println("Current room temperature: ");
     Serial.print(temperature);
     Serial.println(" *C");
     
@@ -381,7 +381,7 @@ void loop() {
      */
       desiredTemp=day_night_temp;
       Serial.println("Desired temp for day_night with starttime and endtime");
-      Serial.print(desiredTemp);
+      Serial.println(desiredTemp);
        Serial.println(" *C");
       Serial.println(startTimeInput);
        Serial.println(endTimeInput);
